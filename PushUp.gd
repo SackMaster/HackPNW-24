1d7273b9cd1fc50104194dd60f48e4a0848125a3
TheGittedGuy <134035645+TheGittedGuy@users.noreply.github.com>
2024-03-02 18:53:34 -0800

    nuh huhbruh
    fuck you

--- PushUp.gd
+++ PushUp.gd
@@ -1,8 +1,13 @@
 extends RigidBody2D
 
+var hor = [-4, -3, -2, -1, 0, 1, 2, 3 ,4]
+var vert = [20, 10, 0, -20, -40, -60]
+
 func _ready():
+	var vertFor = vert.pick_random()
+	var horFor = hor.pick_random()
 	print(self.global_position)
-	apply_impulse(Vector2(0, -1000))
+	apply_impulse(Vector2(50 * horFor, -900 + vertFor))
 	
 #func _physics_process(delta):
 	
